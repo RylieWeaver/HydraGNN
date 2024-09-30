a75a999c9d03630f2fa7ca6e80934bb4f9a01002
Rylie Weaver <rylieweaver9@gmail.com>
2024-09-30 13:25:53 -0400

    create test model

--- hydragnn/models/EGCLStack.py
+++ hydragnn/models/EGCLStack.py
@@ -12,10 +12,12 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch_geometric.nn import Sequential
 from .Base import Base
 
 from ..utils import unsorted_segment_mean
+
 
 
 class EGCLStack(Base):
@@ -46,6 +48,7 @@ class EGCLStack(Base):
             self.feature_layers.append(nn.Identity())
 
     def get_conv(self, input_dim, output_dim, last_layer=False):
+        input_dim=118
         egcl = E_GCL(
             input_channels=input_dim,
             output_channels=output_dim,
@@ -83,6 +86,113 @@ class EGCLStack(Base):
             }
 
         return conv_args
+    
+    def _multihead(self):
+        ############multiple heads/taks################
+        # shared dense layers for heads with graph level output
+        dim_sharedlayers = 0
+        if "graph" in self.config_heads:
+            denselayers = []
+            dim_sharedlayers = self.config_heads["graph"]["dim_sharedlayers"]
+            denselayers.append(nn.Linear(self.hidden_dim, dim_sharedlayers))
+            denselayers.append(self.activation_function)
+            for ishare in range(self.config_heads["graph"]["num_sharedlayers"] - 1):
+                denselayers.append(nn.Linear(dim_sharedlayers, dim_sharedlayers))
+                denselayers.append(self.activation_function)
+            self.graph_shared = nn.Sequential(*denselayers)
+
+        if "node" in self.config_heads:
+            self.num_conv_layers_node = self.config_heads["node"]["num_headlayers"]
+            self.hidden_dim_node = self.config_heads["node"]["dim_headlayers"]
+            self._init_node_conv()
+
+        inode_feature = 0
+        for ihead in range(self.num_heads):
+            # mlp for each head output
+            if self.head_type[ihead] == "graph":
+                num_head_hidden = self.config_heads["graph"]["num_headlayers"]
+                dim_head_hidden = self.config_heads["graph"]["dim_headlayers"]
+                denselayers = []
+                denselayers.append(nn.Linear(dim_sharedlayers, dim_head_hidden[0]))
+                denselayers.append(self.activation_function)
+                for ilayer in range(num_head_hidden - 1):
+                    denselayers.append(
+                        nn.Linear(dim_head_hidden[ilayer], dim_head_hidden[ilayer + 1])
+                    )
+                    denselayers.append(self.activation_function)
+                denselayers.append(
+                    nn.Linear(
+                        dim_head_hidden[-1],
+                        self.head_dims[ihead] * (1 + self.var_output),
+                    )
+                )
+                head_NN = nn.Sequential(*denselayers)
+            elif self.head_type[ihead] == "node":
+                self.node_NN_type = self.config_heads["node"]["type"]
+                head_NN = nn.ModuleList()
+                if self.node_NN_type == "mlp" or self.node_NN_type == "mlp_per_node":
+                    self.num_mlp = 1 if self.node_NN_type == "mlp" else self.num_nodes
+                    assert (
+                        self.num_nodes is not None
+                    ), "num_nodes must be positive integer for MLP"
+                    # """if different graphs in the dataset have different size, one MLP is shared across all nodes """
+                    head_NN = MLPNode(
+                        self.hidden_dim,
+                        self.head_dims[ihead] * (1 + self.var_output),
+                        self.num_mlp,
+                        self.hidden_dim_node,
+                        self.config_heads["node"]["type"],
+                        self.activation_function,
+                    )
+                elif self.node_NN_type == "conv":
+                    for conv, batch_norm in zip(
+                        self.convs_node_hidden, self.batch_norms_node_hidden
+                    ):
+                        head_NN.append(conv)
+                        head_NN.append(batch_norm)
+                    head_NN.append(self.convs_node_output[inode_feature])
+                    head_NN.append(self.batch_norms_node_output[inode_feature])
+                    inode_feature += 1
+                else:
+                    raise ValueError(
+                        "Unknown head NN structure for node features"
+                        + self.node_NN_type
+                        + "; currently only support 'mlp', 'mlp_per_node' or 'conv' (can be set with config['NeuralNetwork']['Architecture']['output_heads']['node']['type'], e.g., ./examples/ci_multihead.json)"
+                    )
+            else:
+                raise ValueError(
+                    "Unknown head type"
+                    + self.head_type[ihead]
+                    + "; currently only support 'graph' or 'node'"
+                )
+            self.heads_NN.append(head_NN)
+    
+    def loss(useless1, pred, value, useless2):
+        # Reshape 'value' to [batch_size, 3] where each row is a one-hot encoding of the class
+        value = value.view(-1, 3).long()
+
+        # Convert the one-hot encoding into class indices using argmax
+        value = torch.argmax(value, dim=1)
+
+        # Apply cross-entropy loss
+        # 'pred[0]' should have shape [batch_size, num_classes] and 'value' should have shape [batch_size]
+        class_weights = torch.tensor([1.0, 8.0, 8.0])  # Adjust the weights based on the class distribution
+        loss = F.cross_entropy(pred[0], value, weight=class_weights)
+       
+        # Calculate overall predictions
+        predictions = torch.argmax(pred[0], dim=1)
+        # Calculate accuracy for each class
+        class_accuracies = []
+        for i in range(3):  # Assuming 3 classes
+            mask = (value == i)  # Identify samples of class 'i'
+            if mask.sum() > 0:  # Avoid division by zero
+                accuracy = (predictions[mask] == value[mask]).float().mean()
+            else:
+                accuracy = torch.tensor(0.0, device=pred[0].device)  # If no samples for this class, accuracy is 0
+            class_accuracies.append(accuracy)
+            print(f"Accuracy for class {i}: {accuracy.item()}")
+
+        return loss, [loss]
 
     def __str__(self):
         return "EGCLStack"
@@ -203,12 +313,12 @@ class E_GCL(nn.Module):
 
     def coord_model(self, coord, edge_index, coord_diff, edge_feat):
         row, col = edge_index
-        trans = coord_diff * self.coord_mlp(edge_feat)
-        trans = torch.clamp(
-            trans, min=-100, max=100
-        )  # This is never activated but just in case it case it explosed it may save the train
-        agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
-        coord = coord + agg * self.coords_weight
+        # trans = coord_diff * self.coord_mlp(edge_feat)
+        # trans = torch.clamp(
+        #     trans, min=-100, max=100
+        # )  # This is never activated but just in case it case it explosed it may save the train
+        # agg = unsorted_segment_mean(trans, row, num_segments=coord.size(0))
+        # coord = coord + agg * self.coords_weight
         return coord
 
     def coord2radial(self, edge_index, coord):
@@ -243,3 +353,68 @@ def unsorted_segment_sum(data, segment_ids, num_segments):
     segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
     result.scatter_add_(0, segment_ids, data)
     return result
+
+
+
+class MLPNode(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        num_mlp,
+        hidden_dim_node,
+        node_type,
+        activation_function,
+    ):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.node_type = node_type
+        self.num_mlp = num_mlp
+        self.activation_function = activation_function
+
+        self.mlp = nn.ModuleList()
+        for _ in range(self.num_mlp):
+            denselayers = []
+            denselayers.append(nn.Linear(self.input_dim, hidden_dim_node[0]))
+            denselayers.append(self.activation_function)
+            for ilayer in range(len(hidden_dim_node) - 1):
+                denselayers.append(
+                    nn.Linear(hidden_dim_node[ilayer], hidden_dim_node[ilayer + 1])
+                )
+                denselayers.append(self.activation_function)
+            denselayers.append(nn.Linear(hidden_dim_node[-1], output_dim))
+            denselayers.append(nn.Softmax(dim=1))  # New Line for classification task of chirality
+            self.mlp.append(nn.Sequential(*denselayers))
+
+    def node_features_reshape(self, x, batch):
+        """reshape x from [batch_size*num_nodes, num_features] to [batch_size, num_features, num_nodes]"""
+        num_features = x.shape[1]
+        batch_size = batch.max() + 1
+        out = torch.zeros(
+            (batch_size, num_features, self.num_nodes),
+            dtype=x.dtype,
+            device=x.device,
+        )
+        for inode in range(self.num_nodes):
+            inode_index = [i for i in range(inode, batch.shape[0], self.num_nodes)]
+            out[:, :, inode] = x[inode_index, :]
+        return out
+
+    def forward(self, x: torch.Tensor, batch: torch.Tensor):
+        if self.node_type == "mlp":
+            outs = self.mlp[0](x)
+        else:
+            outs = torch.zeros(
+                (x.shape[0], self.output_dim),
+                dtype=x.dtype,
+                device=x.device,
+            )
+            x_nodes = self.node_features_reshape(x, batch)
+            for inode in range(self.num_nodes):
+                inode_index = [i for i in range(inode, batch.shape[0], self.num_nodes)]
+                outs[inode_index, :] = self.mlp[inode](x_nodes[:, :, inode])
+        return outs
+
+    def __str__(self):
+        return "MLPNode"
