19666d3288d8bd294183d4975034f76d74970eb2
Rylie Weaver <rylieweaver9@gmail.com>
2024-12-14 00:42:29 -0500

    merge upstream main

--- tests/test_deepspeed.py
+++ tests/test_deepspeed.py
@@ -1,6 +1,6 @@
-import pytest
+# import pytest
 
-from tests.test_graphs import unittest_train_model
+# from tests.test_graphs import unittest_train_model
 
 
 # Test vector output
@@ -43,25 +43,35 @@ def pytest_train_model_vectoroutput_w_deepspeed_global_attention(
 # # Test deepspeed zero
 # # cannot use deepspeed zero-optimizer with gloo backend on the server
 # @pytest.mark.parametrize("model_type", ["PNA"])
-# @pytest.mark.parametrize("zero_stage", [1, 2, 3])
 # @pytest.mark.mpi
-# def pytest_train_model_vectoroutput_w_deepspeed_zero(
-#     model_type, zero_stage, overwrite_data=False
-# ):
-#     overwrite_config = {
-#         "NeuralNetwork": {
-#             "ds_config": {
-#                 "zero_optimization": {
-#                     "stage": zero_stage,
-#                 }
-#             }
-#         }
-#     }
+# def pytest_train_model_vectoroutput_w_deepspeed(model_type, overwrite_data=False):
 #     unittest_train_model(
-#         model_type,
-#         f"ci_vectoroutput.json",
-#         True,
-#         overwrite_data,
-#         use_deepspeed=True,
-#         overwrite_config=overwrite_config,
+#         model_type, "ci_vectoroutput.json", True, overwrite_data, use_deepspeed=True
 #     )
+
+
+# # # Test deepspeed zero
+# # # cannot use deepspeed zero-optimizer with gloo backend on the server
+# # @pytest.mark.parametrize("model_type", ["PNA"])
+# # @pytest.mark.parametrize("zero_stage", [1, 2, 3])
+# # @pytest.mark.mpi
+# # def pytest_train_model_vectoroutput_w_deepspeed_zero(
+# #     model_type, zero_stage, overwrite_data=False
+# # ):
+# #     overwrite_config = {
+# #         "NeuralNetwork": {
+# #             "ds_config": {
+# #                 "zero_optimization": {
+# #                     "stage": zero_stage,
+# #                 }
+# #             }
+# #         }
+# #     }
+# #     unittest_train_model(
+# #         model_type,
+# #         f"ci_vectoroutput.json",
+# #         True,
+# #         overwrite_data,
+# #         use_deepspeed=True,
+# #         overwrite_config=overwrite_config,
+# #     )
